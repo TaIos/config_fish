2d077e6c080247ca929de5209f866752cb7cf84c
Martin Šafránek <gismocz@gmail.com>
2021-02-28 18:30:50 +0100

    Add debugging options for PDP

--- config.fish
+++ config.fish
@@ -34,6 +34,8 @@ alias amendall='git add .;git commit --amend --no-edit'
 
 alias gxx='g++ -Wall -pedantic -Wextra --std=c++11 -o run.out'
 alias pdp='g++ -Wall -pedantic -Wextra --std=c++11 -O3 -funroll-loops -o run.out'
+alias pdpdeb='g++ -Wall -pedantic -Wextra --std=c++11 -O3 -funroll-loops -g -o run.out'
+alias callgrind='valgrind --tool=callgrind --dump-instr=yes --simulate-cache=yes --collect-jumps=yes'
 
 alias efish='vim ~/.config/fish/config.fish'
 
